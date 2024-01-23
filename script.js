de2e5794cb819b03ac2563af9c055ddf89596267
Dev-Freelas <157433453+Dev-Freelas@users.noreply.github.com>
2024-01-23 19:18:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,13 +10,11 @@ function sendMail() {
 
     const service = "service_6j8htku";
     const template = "template_65bu28c";
-    const captchaToken = grecaptcha.getResponse();
 
     const params = {
         message: document.querySelector("#contact_message").value,
         sendername: document.querySelector("#contact_username").value,
         reply: document.querySelector("#contact_reply").value,
-        "g-recaptcha-response": captchaToken,
     }
 
     emailjs.send(service, template, params)
@@ -26,11 +24,6 @@ function sendMail() {
         .catch(err => {
             console.error(err);
             sendContactFormBtn.textContent = "Erro ao enviar mensagem";
-        })
-        .success(function () {
-            setTimeout(function () {
-                sendContactFormBtn.textContent = "Enviar serviço";
-            }, 1000);
         });
 
     contactForm.reset();
